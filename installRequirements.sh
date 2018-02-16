d00083ff761699cf2973c9d69e4c7dd6048e859e
ariglenn <ariisawesome22@gmail.com>
2018-02-15 19:53:43 -0800

    Adding 2

---
+++ installRequirements.sh
@@ -0,0 +1,14 @@
+#ssh pi@192.168.1.119 -> Pass: Raspberry
+
+#install Github
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install git
+git clone 'https://github.com/AriGlenn/RaspberryPi_LED.git'
+#ariglenn -> Pass:
+
+#install Flask
+sudo apt-get install python-flask
+
+
+#install Python
