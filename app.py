7a0449f196abee093911bef1b3c076329f3166cf
ariglenn <ariisawesome22@gmail.com>
2018-02-15 19:37:46 -0800

    Adding leds 2

--- app.py
+++ app.py
@@ -18,21 +18,21 @@ print("Running")
 @app.route('/')
 def index():
     print('Running...')
-    GPIO.output(21,GPIO.HIGH)
+    GPIO.output(21,GPIO.LOW)
     return render_template('index.html')
 
 @app.route('/ledOFF')
 def LedOff():
     print('off')
     GPIO.output(21,GPIO.LOW)
-    GPIO.output(ledNumber,False)
+    #GPIO.output(ledNumber,False)
     return 'LED off'
 
 @app.route('/ledON')
 def LedOn():
     print('on')
-    GPIO.output(ledNumber,True)
     GPIO.output(21,GPIO.HIGH)
+    #GPIO.output(ledNumber,True)
     return 'LED on'
 
 
